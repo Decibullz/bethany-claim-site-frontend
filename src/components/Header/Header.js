188a7cc771ebda2439861659169f3da55ff19394
Cody Snell <codyjsnell@yahoo.com>
2021-01-30 01:15:24 -0600

    corrects links and h1 tag

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,13 +6,13 @@ export default function Header(props){
     return(
         <header className="Header">
             <Link to = '/'>
-            <h1>Book-It</h1>
+            <h1>Bethany Senior Living lost and found</h1>
             </Link>
             <nav>
                 <ul className="NavLinks">
                     
             <li>{props.user && <Link to='' onClick={props.handleLogout}>Log Out</Link>}</li>
-            <li>{props.user && <Link to= '/search'>Search</Link>}</li>                   
+            <li>{props.user && <Link to= '/add'>Add Item</Link>}</li>                   
             <li>{!props.user && <Link to= '/signup'>Signup</Link>}</li>
             <li>{!props.user && <Link to ='/login'>Login</Link>}</li>                        
                  
