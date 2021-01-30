027fb502fe42674f43cc6238af8e7959b86af9be
Cody Snell <codyjsnell@yahoo.com>
2021-01-30 01:16:25 -0600

    adds delete function

--- src/services/itemService.js
+++ src/services/itemService.js
@@ -1,3 +1,15 @@
 export function getItems(items){
     return fetch('http://localhost:3000/items').then(res=>res.json())
 }
+
+export function deleteItem(item){
+    console.log(item)
+    return fetch('http://localhost:3000/items/delete', {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'Application/json'
+        },
+       body: JSON.stringify(item)
+       
+    })
+}
