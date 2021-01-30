22cbbe782f99b5e6ebdabf3fe343834c8b0b73ef
Cody Snell <codyjsnell@yahoo.com>
2021-01-30 01:14:06 -0600

    adds protected delete button

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,9 +1,22 @@
+import {deleteItem} from '../services/itemService'
+
+
 export default function Item(props){
+    const deleteOneItem = async (id) => {
+
+        await deleteItem({
+          itemId: id
+        })
+        window.location.reload(false)
+      }
+
+
     return( 
         <div className="Item">
             <h1>{`${props.item.name}`}</h1>
             <img src={`${props.item.image}`}/>
             <p>{`${props.item.description}`}</p>
+            {props.user && <button onClick={()=>deleteOneItem(props.item._id)}>delete</button>}
         </div>
     )
 }
