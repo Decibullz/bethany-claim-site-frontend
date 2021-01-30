6773ffe469920ebf3367c5031bba397437e7826e
Cody Snell <codyjsnell@yahoo.com>
2021-01-30 01:15:47 -0600

    styling changes and passes props

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,14 +4,12 @@ import Item from "../components/Item";
 export default function HomePage(props){
     return(
         <main className="Page HomePage">
-            <div className="transbox">
-                
-                <h2>Please call 361-541-2429 to claim</h2>
-                {!props.user && <h5>Please <Link to='/signup'>Signup</Link> or <Link to='/login'>Login</Link></h5>}
+            <div className="">
+                <h1>Please call 361-541-2429 to claim</h1>
             </div>
             <div className="collection">
              {props.itemData.results.items && props.itemData.results.items.map((item,idx)=>
-                <Item key={idx} item={item}/>)}
+                <Item user={props.user}key={idx} item={item}/>)}
             </div>
             </main> 
     )
