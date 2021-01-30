9bad4986dd503804c0d05a27c98f034629ef27a2
Cody Snell <codyjsnell@yahoo.com>
2021-01-30 01:14:19 -0600

    adds ability to add item

---
+++ src/components/AddItem/AddItem.js
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+
+
+export default function AddItem(props){
+
+    const [formState , setFormState ]= useState({
+        name:"",
+        image:"",
+        description:""
+    })
+
+    function handleChange(evt) {
+        setFormState((prevState)=>({
+          ...prevState,
+          [evt.target.name]:evt.target.value
+        }))
+      }
+
+      function addNewItem(lostItem) {
+        const BASE_URL = 'http://localhost:3000/items'
+        return fetch(BASE_URL + '/save', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'Application/json'
+            },
+            body: JSON.stringify(lostItem)
+        }).then(response => {
+            if(response.ok) return response.json();
+    }).then(setFormState({
+        name:"",
+        image:"",
+        description:""
+    })).then(alert('Item Added Successfully!'))}
+
+      async function handleSubmit(evt) {
+        evt.preventDefault();
+        try {
+          await addNewItem(formState)
+        } catch (err) {
+            console.log(err);
+            alert(err.message);
+        }
+      }
+
+    function formValid(){
+        return !!(formState.name && formState.description && formState.image)
+    }
+
+
+    return(
+        <main className="Page">
+            <div className="transbox">
+            <h1>
+            Add Item
+            </h1>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <div className="col-sm-12">
+                        <input value={formState.name} name="name" placeholder='Name' className = "form-control" onChange={handleChange} type="text"/>
+                    </div>
+                </div>
+                <div className="form-group">
+                    <div className="col-sm-12">
+                        <input value={formState.image} name = "image" placeholder='image' className = "form-control" onChange={handleChange} type="text"/>
+                    </div>
+                </div>
+                <div className="form-group">
+                    <div className="col-sm-12">
+                        <input value={formState.description} name = "description" placeholder='description' className = "form-control" onChange={handleChange} type="text"/>
+                    </div>
+                </div>
+                <div className="form-group">
+                    <div className = "col-sm-12">
+                        <input disabled= {!formValid()} value = 'Add Item' className = "form-control" type="submit"/>
+                        <Link to= '/' >Cancel</Link>
+                    </div>
+                </div>
+            </form>
+                </div>
+            </main>
+    )
+}
