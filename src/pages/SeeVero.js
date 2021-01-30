ef74145573e8e2b0792cde506165631a1a7725a3
Cody Snell <codyjsnell@yahoo.com>
2021-01-30 01:15:56 -0600

    creates component

---
+++ src/pages/SeeVero.js
@@ -0,0 +1,9 @@
+export default function SeeVero(){
+    return(
+        <main className="Page ">
+            <div>
+                <h2>Please see Veronica Snell to Register</h2>
+            </div>
+            </main> 
+    )
+}
