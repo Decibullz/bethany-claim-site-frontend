bb5b4c08e3b039ad79311324e63d72a3df886c10
Cody Snell <codyjsnell@yahoo.com>
2021-01-29 23:10:15 -0600

    adds homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import Item from "../components/Item";
 
-function HomePage() {
-    return (
-        <div>
-            
-        </div>
+export default function HomePage(props){
+    return(
+        <main className="Page HomePage">
+            <div className="transbox">
+                
+                <h2>Please call 361-541-2429 to claim</h2>
+                {!props.user && <h5>Please <Link to='/signup'>Signup</Link> or <Link to='/login'>Login</Link></h5>}
+            </div>
+            <div className="collection">
+             {props.itemData.results.items && props.itemData.results.items.map((item,idx)=>
+                <Item key={idx} item={item}/>)}
+            </div>
+            </main> 
     )
 }
-
-export default HomePage
