a313dc1540e6b7b9425afad79ea8fe02b5c9f2cc
Cody Snell <codyjsnell@yahoo.com>
2021-01-31 18:37:12 -0600

    fixes link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ export default function Footer(props){
     return(
         <footer className="Footer">
             <p>
-            Site Made by <a href="codysnell.dev">Cody Snell</a> Copyright &copy; {new Date().getFullYear()} All Rights Reserved 
+            Site Made by <a href="https://www.codysnell.dev" target ='_blank' rel="noreferrer" >Cody Snell</a> Copyright &copy; {new Date().getFullYear()} All Rights Reserved 
             </p>
         </footer>
     )
