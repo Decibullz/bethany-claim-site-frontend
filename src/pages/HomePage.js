7b3aa20d369f7b7dd24294909e7e0a30b09905a5
Cody Snell <codyjsnell@yahoo.com>
2021-01-31 18:37:31 -0600

    changes header and adds contact component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,18 @@
 import { Link } from "react-router-dom";
+import Contact from "../components/Contact/Contact";
 import Item from "../components/Item";
 
 export default function HomePage(props){
     return(
         <main className="Page HomePage">
             <div className="">
-                <h1>Please call 361-541-2429 to claim</h1>
+                <h1>Please fill out form at bottom of page to claim items</h1>
             </div>
             <div className="collection">
              {props.itemData.results.items && props.itemData.results.items.map((item,idx)=>
                 <Item user={props.user}key={idx} item={item}/>)}
             </div>
+            <Contact/>
             </main> 
     )
 }
